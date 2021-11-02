5573f000ef44419e25d37f855a4ee392917cac19
n8paternoster <n8paternoster@gmail.com>
2021-11-01 21:28:00 -0400

    Add project files.

---
+++ js/nate.js
@@ -0,0 +1,36 @@
+// JavaScript source code
+
+
+/* ---- Open and close accordion panel (coursework.html) ---- */
+function toggleAccordion(element) {
+
+    /* Toggle between adding and removing the "accordion-active" class, to highlight the button that controls the panel */
+    element.classList.toggle("accordion-active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = element.nextElementSibling;
+    if (panel.style.maxHeight) {    // max height is not 0
+        panel.style.maxHeight = null;
+    } else {                        // max height is 0
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+}
+
+/* ---- Make card elements clickable while allowing embedded links to also be clickable ---- */
+var cardList = document.querySelectorAll(".card");
+cardList.forEach(function (card) {
+
+    // prevent double event triggering on links inside the card
+    var clickableElements = Array.from(card.querySelectorAll(".card-clickable"));
+    clickableElements.forEach((ele) => ele.addEventListener("click", (e) => e.stopPropagation()));
+
+    // add a click event to the whole card
+    var cardLink = card.querySelector(".card-link");
+    console.log(card, cardLink);
+    card.addEventListener("click", function () {
+        var noTextSelected = !window.getSelection().toString();
+        if (noTextSelected) {   // allow text highlighting without triggering redirect
+            cardLink.click();
+        }
+    });
+});
