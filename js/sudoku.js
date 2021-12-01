008c06ff3d73c329378a3f4ec948743b2d7b70ed
n8paternoster <n8paternoster@gmail.com>
2021-11-30 22:19:30 -0500

    board changes, add sudoku.js

---
+++ js/sudoku.js
@@ -0,0 +1,13 @@
+﻿var sudokuBoard = document.getElementById("sudoku-board-container");
+
+/* prevent non-numeric input from displaying, delegate event to sudokuBoard */
+sudokuBoard.addEventListener("keydown", function (e) {
+    if (e.key.length === 1 && /\D/.test(e.key) && !e.ctrlKey) {
+        e.preventDefault();
+    }
+});
+
+/* prevent invalid numeric from displaying, delegate event to sudokuBoard */
+sudokuBoard.addEventListener("change", function (e) {
+    if (e.target.value > 9) e.target.value = "";
+});
