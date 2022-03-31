70ca4039cc54d84270bb19a4c264ddac105f6f4e
n8paternoster <n8paternoster@gmail.com>
2022-03-31 00:31:02 -0400

    .

--- js/audio-analyzer.js
+++ js/audio-analyzer.js
@@ -10,13 +10,17 @@ var canvas = document.getElementById('audio-visualizer');
 var canvasCtx = canvas.getContext('2d');
 /* set the board canvas element size */
 if (canvas) {
-    let canvasRect = canvas.getBoundingClientRect();
+    let canvasRect = document.getElementById('canvas-container').getBoundingClientRect();
     canvas.style.width = canvasRect.width + "px";
     canvas.style.height = canvasRect.height + "px";
     var scale = window.devicePixelRatio;
     canvas.width = Math.floor(canvasRect.width * scale);
     canvas.height = Math.floor(canvasRect.height * scale);
     canvasCtx.scale(scale, scale);
+    console.log("Canvas style height: ");
+    console.log(canvas.style.height);
+    console.log("Canvas height: ");
+    console.log(canvas.height);
 }
 
 analyzer.fftSize = 2048;
@@ -26,32 +30,40 @@ var data = new Float32Array(bufferSize);
 
 var drawHandle;
 function draw() {
-    analyzer.getFloatTimeDomainData(data);
+    var rect = document.getElementById('canvas-container').getBoundingClientRect();
+    var width = rect.width;
+    var height = rect.height;
 
     canvasCtx.fillStyle = "rgb(230, 230, 230)";
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+    canvasCtx.fillRect(0, 0, width, height);
 
     canvasCtx.lineWidth = 1;
     canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+
+    var rect = document.getElementById('canvas-container').getBoundingClientRect();
+    var width = rect.width;
+    var height = rect.height;
+
+    analyzer.getFloatTimeDomainData(data);
 
     canvasCtx.beginPath();
 
     // move to the first sample
     var x = 0;
-    var y = (canvas.height / 2) * (1 - data[0]);
+    var y = (height / 2) * (1 - data[0]);
     canvasCtx.moveTo(x, y);
 
     // line to each subsequent sample
-    var deltaX = canvas.width / bufferSize;
+    var deltaX = width / bufferSize;
     for (let i = 1; i < bufferSize; i++) {
         x += deltaX;
-        y = (canvas.height / 2) * (1 - data[i]);
+        y = (height / 2) * (1 - data[i]);
         canvasCtx.lineTo(x, y);
         if (data[i] >= 1 || data[i] <= -1) console.log(data[i]);
     }
 
     // end on the midpoint
-    canvasCtx.lineTo(canvas.width, canvas.height / 2);
+    canvasCtx.lineTo(width, height / 2);
     canvasCtx.stroke();
 
     drawHandle = requestAnimationFrame(draw);
