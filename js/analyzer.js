f591f44b67e9e60a924f822d3f23238b3e37d2cc
n8paternoster <n8paternoster@gmail.com>
2022-04-05 23:31:16 -0400

    reorganized

--- js/analyzer.js
+++ js/analyzer.js
@@ -1,8 +1,6 @@
-﻿// use JS naming convensions. No_under_scores
-// Caps are for Constructors
+﻿// Functions to achieve "pixel-perfect" canvas rendering; this is necessary since animating the waveform involves many consecutive redraws of the waveform canvas and even a tiny fraction mismatch between the css pixel size and the device's physical pixel size will be amplified and result in various aliasing problems
 function clientToDeviceRect(cssRect) {
     const dpr = window.devicePixelRatio;
-
     const deviceRect = {};
     for (const k in cssRect) {
         if (k == 'toJSON') continue;
@@ -10,18 +8,18 @@ function clientToDeviceRect(cssRect) {
     }
     return deviceRect;
 }
-function snapToDeviceRect(e, deviceRect) {
+function snapToDeviceRect(ele, deviceRect) {
     const dpr = window.devicePixelRatio;
-    e.style.position = 'absolute';
-    e.style.top = deviceRect.top % 1 / dpr + 'px';
-    e.style.left = deviceRect.left % 1 / dpr + 'px';
-    e.style.width = deviceRect.width / dpr + 'px';
-    e.style.height = deviceRect.height / dpr + 'px';
+    ele.style.position = 'absolute';
+    elee.style.top = deviceRect.top % 1 / dpr + 'px';
+    ele.style.left = deviceRect.left % 1 / dpr + 'px';
+    ele.style.width = deviceRect.width / dpr + 'px';
+    ele.style.height = deviceRect.height / dpr + 'px';
 }
-function snapToParentPixels(e) {
-    const clientRect = e.parentNode.getBoundingClientRect();
+function snapToParentPixels(ele) {
+    // fallback for devices that don't support 'device-pixel-content-box' (accessing an element's size in device pixels directly)
+    const clientRect = ele.parentNode.getBoundingClientRect();
     const deviceRect = clientToDeviceRect(clientRect);
-
     // Chrome doesn't snap non-DPR-multiple canvases properly, snapping only to evens.
     const evenOnly = false;
     if (evenOnly) {
@@ -34,46 +32,8 @@ function snapToParentPixels(e) {
             deviceRect[k] = cur;
         }
     }
-
-    snapToDeviceRect(e, deviceRect);
-
+    snapToDeviceRect(ele, deviceRect);
     return deviceRect;
-}
-
-const canvases = document.querySelectorAll('canvas');
-let supportsDevicePixelContextBox = true;
-const resizeObserver = new ResizeObserver(onResize);
-for (const canvas of canvases) {
-    canvas.parentNode.canvas = canvas;
-    if (supportsDevicePixelContextBox) {
-        try {
-            resizeObserver.observe(canvas.parentNode, { box: 'device-pixel-content-box' });
-        } catch (e) {
-            supportsDevicePixelContextBox = false;
-        }
-    }
-    if (!supportsDevicePixelContextBox) {
-        resizeObserver.observe(canvas.parentNode, { box: 'border-box' });
-    }
-}
-// the problem here is if we don't support `device-pixel-content-box`
-// then zooming the page will not notify us of canvases who's CSS size
-// has not changed even though the number of native pixels they cover has.
-if (!supportsDevicePixelContextBox) {
-    window.addEventListener('resize', () => {
-        for (const canvas of canvases) {
-            resizeCanvasToParentPixels(canvas);
-            draw(canvas);
-        }
-    });
-}
-
-function onResize(entries) {
-    for (const entry of entries) {
-        const canvas = entry.canvas;
-        resizeCanvasToDevicePixels(entry);
-        draw(entry.target.canvas);
-    }
 }
 function resizeCanvasToDevicePixels(entry) {
     const canvas = entry.target.canvas;
@@ -89,53 +49,105 @@ function resizeCanvasToParentPixels(canvas) {
     canvas.width = rect.width;
     canvas.height = rect.height;
 }
-function draw(canvas) {
+function redrawCanvas(canvas) {
     const ctx = canvas.getContext('2d');
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // set the background
-    ctx.fillStyle = "rgb(230, 230, 230)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    if (canvas.id === "waveform-layer") {
+        // prevent browser anti-aliasing which will distort the copied data
+        ctx.mozImageSmoothingEnabled = false;
+        ctx.webkitImageSmoothingEnabled = false;
+        ctx.msImageSmoothingEnabled = false;
+        ctx.imageSmoothingEnabled = false;
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "blue";
+    } else if (canvas.id === "grid-layer") {
+        drawGrid();
+    }
+}
+function onResize(entries) {
+    for (const entry of entries) {
+        resizeCanvasToDevicePixels(entry);
+        redrawCanvas(entry.target.canvas);
+    }
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var audioEle = document.getElementById('audio-source');
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var analyzer = new AnalyserNode(audioCtx);
 const bufferSize = 512; // lower = faster fps, smoother waveform scrolling
-var visibleFrames = 1;
-analyzer.fftSize = bufferSize;
 var buffer = new Float32Array(bufferSize);
-
-// connect audio element
-var source = audioCtx.createMediaElementSource(audioEle);
-source.connect(analyzer);
-analyzer.connect(audioCtx.destination);
-
+var visibleFrames = 1;
 var fpsInterval, now, then, elapsed;
 var waveformCanvas = document.getElementById('waveform-layer');
 var waveformCtx = waveformCanvas.getContext('2d');
-function drawWaveform() {
+var drawHandle;
+var prevSample = 0;
+
+function drawGrid(sampleRate = 44100) {
+    var canvas = document.getElementById('grid-layer');
+    var ctx = canvas.getContext('2d');
+    var width = canvas.width;
+    var height = canvas.height;
+
+    // set the background
+    ctx.fillStyle = "rgb(230, 230, 230)";
+    ctx.fillRect(0, 0, width, height);
+
+    // draw the border
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(1, 1, width - 2, height - 2);
+
+    // draw the x-axis
+    var windowLength = (bufferSize * visibleFrames) / sampleRate * 1000;    // in msecs
+    var tDelta = 1000; // in msecs
+    if (windowLength <= 20) tDelta = 1;
+    else if (windowLength <= 50) tDelta = 2.5;
+    else if (windowLength <= 100) tDelta = 5;
+    else if (windowLength <= 200) tDelta = 10;
+    else if (windowLength <= 500) tDelta = 25;
+    else if (windowLength <= 1000) tDelta = 50;
+    else if (windowLength <= 2000) tDelta = 100;
+    else if (windowLength <= 5000) tDelta = 250;
+    else if (windowLength <= 10000) tDelta = 500;
+    else tDelta = 1000;
+    var numNotches = Math.floor(windowLength / tDelta);
+    ctx.fillStyle = 'black';
+    ctx.font = 'bold 10px sans-serif';
+    ctx.textAlign = 'center';
+    ctx.beginPath();
+    for (let i = 1; i < numNotches; i++) {  // notches
+        ctx.moveTo(i * (width / numNotches), height - 7);
+        ctx.lineTo(i * (width / numNotches), height + 7);
+        ctx.fillText((tDelta * (i - numNotches)).toFixed(2), i * (width / numNotches), height + 20);
+    }
+    ctx.stroke();
+
+
+    // draw the y-axis
+    ctx.beginPath();
+    for (let i = 1; i < 10; i++) {
+        if (i === 5) continue;
+        ctx.moveTo(width - 7, i * (height / 10));
+        ctx.lineTo(width + 7, i * (height / 10));
+    }
+    ctx.stroke();
+}
+function drawWaveform(sampleRate = 44100) {
     // prevent browser anti-aliasing which will distort the copied data
     waveformCtx.mozImageSmoothingEnabled = false;
     waveformCtx.webkitImageSmoothingEnabled = false;
     waveformCtx.msImageSmoothingEnabled = false;
     waveformCtx.imageSmoothingEnabled = false;
-
-    // TODO - change this?
-    analyzer.smoothingTimeConstant = 0;
-
     waveformCtx.lineWidth = 2;
     waveformCtx.strokeStyle = "blue";
-    fpsInterval = 1000 / (44100 / bufferSize);  // fps = sampleRate/bufferSize
-    console.log(fpsInterval);
+
+    // throttle RAF to capture the as many audio samples as possible
+    fpsInterval = 1000 / (sampleRate / bufferSize);  // fps = sampleRate/bufferSize
     then = window.performance.now();
     drawHandle = drawFrame();
 }
-var drawHandle;
-var prevSample = 0;
 function drawFrame(timestamp) {
     drawHandle = requestAnimationFrame(drawFrame);
     now = timestamp;
@@ -154,6 +166,7 @@ function drawFrame(timestamp) {
         waveformCtx.globalCompositeOperation = 'source-over';
 
         analyzer.getFloatTimeDomainData(buffer);
+
         // move to the previous sample
         waveformCtx.beginPath();
         var x = width - frameWidth - deltaX;
@@ -172,6 +185,7 @@ function drawFrame(timestamp) {
     }
 }
 
+var audioEle = document.getElementById('audio-source');
 audioEle.addEventListener('play', function () {
     if (audioCtx.state === 'suspended') {
         audioCtx.resume();
@@ -180,4 +194,47 @@ audioEle.addEventListener('play', function () {
 });
 audioEle.addEventListener('pause', function () {
     cancelAnimationFrame(drawHandle);
+});
+document.getElementById('x-scale').addEventListener('input', function (e) {
+    //// exponential scaling
+    //const a = e.target.max;
+    //const b = Math.pow(a, 1 / a);
+    //visibleFrames = Math.floor(e.target.min * Math.pow(b, e.target.value));
+    visibleFrames = e.target.value;
+    drawGrid();
+});
+document.addEventListener('DOMContentLoaded', e => {
+
+    // Observe the canvases on the page to 
+    const canvases = document.querySelectorAll('canvas');
+    let supportsDevicePixelContextBox = true;
+    const resizeObserver = new ResizeObserver(onResize);
+    for (const canvas of canvases) {
+        canvas.parentNode.canvas = canvas;
+        if (supportsDevicePixelContextBox) {
+            try {
+                resizeObserver.observe(canvas.parentNode, { box: 'device-pixel-content-box' });
+            } catch (e) {
+                supportsDevicePixelContextBox = false;
+            }
+        } else {
+            resizeObserver.observe(canvas.parentNode, { box: 'border-box' });
+        }
+    }
+    if (!supportsDevicePixelContextBox) {
+        // if device-pixel-content-box is not supported, then zooming the page will not notify us of canvases whose CSS size has not changed even though the number of native pixels they cover has
+        window.addEventListener('resize', () => {
+            for (const canvas of canvases) {
+                resizeCanvasToParentPixels(canvas);
+                redrawCanvas(canvas);
+            }
+        });
+    }
+
+    // setup audio graph
+    var source = audioCtx.createMediaElementSource(audioEle);
+    source.connect(analyzer);
+    analyzer.connect(audioCtx.destination);
+    analyzer.fftSize = bufferSize;
+    analyzer.smoothingTimeConstant = 0; // TODO - change this?
 });
